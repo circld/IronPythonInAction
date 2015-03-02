490775ff8ae59004112fe146dec184a41377e995
circld <circld1@gmail.com>
2015-03-02 00:04:00 -0500

    Added XML writing to save function. Modularized existing code.

--- MultiDoc/MultiDoc/MultiDoc.py
+++ MultiDoc/MultiDoc/MultiDoc.py
@@ -7,16 +7,18 @@ clr.AddReference('System.Windows.Forms')
 
 from System.Drawing import Bitmap, Color, Size
 from System.Windows.Forms import (
-    Application, DialogResult, DockStyle, Form, 
+    Application, DockStyle, Form, 
     Keys, MenuStrip, MessageBox, MessageBoxButtons,
     MessageBoxDefaultButton, MessageBoxIcon,
-    ScrollBars, SaveFileDialog, TabAlignment, 
+    ScrollBars, TabAlignment, 
     TabControl, TabPage, TextBox,
     ToolStripItemDisplayStyle, ToolStrip,
     ToolStripButton, ToolStripGripStyle,
     ToolStripMenuItem,
 )
-from System.IO import Directory, Path, StreamWriter
+from System.IO import Directory, Path
+# import other from other user-written modules
+from savecommands import SaveCommand, SaveAsCommand
 
 executablePath = __file__
 if executablePath is None:
@@ -198,71 +200,6 @@ class TabController(object):
         self.document[index].text = textBox.Text
 
 # Commands
-filter = 'Text files (*.txt)|*.txt|All files (*.*)|*.*'
-class SaveCommand(object):
-
-    title = 'Save Document'
-
-    def __init__(self, document, tabController):
-        self.document = document
-        self.tabController = tabController
-        self.saveDialog = SaveFileDialog()
-        self.saveDialog.Filter = filter
-        self.saveDialog.Title = self.title
-
-    def execute(self):
-        fileName = self.document.fileName
-        text = self.getText()
-
-        directory = Path.GetDirectoryName(fileName)
-        directoryExists = Directory.Exists(directory)
-        if fileName is None or not directoryExists:
-            self.promptAndSave(text)
-        else:
-            self.saveFile(fileName, text)
-
-    def getText(self):
-        self.tabController.updateDocument()
-        return self.document[0].text
-
-    def promptAndSave(self, text):
-        saveDialog = self.saveDialog
-        if saveDialog.ShowDialog() == DialogResult.OK:
-            fileName = saveDialog.FileName
-            if self.saveFile(fileName, text):
-                self.document.fileName = fileName
-
-    def saveFile(self, fileName, text):
-        try:
-            writer = StreamWriter(fileName)
-            writer.Write(text)
-            writer.Close()
-            return True
-        except IOError, e:
-            name = Path.GetFileName(fileName)
-            MessageBox.Show(
-                'Could not write file %s\r\nThe error was:\r\n%s' %
-                (name, e),
-                'Error Saving File',
-                MessageBoxButtons.OK,
-                MessageBoxIcon.Error
-            )
-            return False
-
-
-class SaveAsCommand(SaveCommand):
-
-    title = "Save Document As"
-
-    def execute(self):
-        fileName = self.document.fileName
-        text = self.getText()
-        ' If fileName exists, set initial dialog directory to fileName directory'
-        if fileName is not None:
-            name = Path.GetFileName(fileName)
-            directory = Path.GetDirectoryName(fileName)
-            self.saveDialog.InitialDirectory = directory
-        self.promptAndSave(text)
 
 
 Application.EnableVisualStyles()
---
+++ MultiDoc/MultiDoc/savecommands.py
@@ -0,0 +1,70 @@
+from System.Windows.Forms import SaveFileDialog, DialogResult
+from System.IO import Directory, Path
+from documentwriter import DocumentWriter
+
+
+filter = 'Text files (*.txt)|*.txt|All files (*.*)|*.*'
+
+class SaveCommand(object):
+
+    title = 'Save Document'
+
+    def __init__(self, document, tabController):
+        self.document = document
+        self.tabController = tabController
+        self.saveDialog = SaveFileDialog()
+        self.saveDialog.Filter = filter
+        self.saveDialog.Title = self.title
+
+    def execute(self):
+        fileName = self.document.fileName
+        text = self.getUpdatedDocument()
+
+        directory = Path.GetDirectoryName(fileName)
+        directoryExists = Directory.Exists(directory)
+        if fileName is None or not directoryExists:
+            self.promptAndSave(text)
+        else:
+            self.saveFile(fileName, text)
+
+    def getUpdatedDocument(self):
+        self.tabController.updateDocument()
+        return self.document
+
+    def promptAndSave(self, text):
+        saveDialog = self.saveDialog
+        if saveDialog.ShowDialog() == DialogResult.OK:
+            fileName = saveDialog.FileName
+            if self.saveFile(fileName, text):
+                self.document.fileName = fileName
+
+    def saveFile(self, fileName, text):
+        try:
+            writer = DocumentWriter(fileName)
+            writer.write(text)
+            return True
+        except IOError, e:
+            name = Path.GetFileName(fileName)
+            MessageBox.Show(
+                'Could not write file %s\r\nThe error was:\r\n%s' %
+                (name, e),
+                'Error Saving File',
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error
+            )
+            return False
+
+
+class SaveAsCommand(SaveCommand):
+
+    title = "Save Document As"
+
+    def execute(self):
+        fileName = self.document.fileName
+        text = self.getUpdatedDocument()
+        ' If fileName exists, set initial dialog directory to fileName directory'
+        if fileName is not None:
+            name = Path.GetFileName(fileName)
+            directory = Path.GetDirectoryName(fileName)
+            self.saveDialog.InitialDirectory = directory
+        self.promptAndSave(text)
